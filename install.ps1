ac9c1aed4f7b927e19d1b3c00750dedbf4c2c22e
oliverdding <oliverdding@outlook.com>
2021-12-04 11:07:36 +0800

    update

--- install.ps1
+++ install.ps1
@@ -8,17 +8,46 @@ New-Item -Path "$env:USERPROFILE\Developer" -ItemType Directory
 
 # Set environment variable
 
-$env:SCOOP = '%AppData%\Scoop'
+$env:XDG_DATA_HOME = "$env:LOCALAPPDATA"
+[Environment]::SetEnvironmentVariable('XDG_DATA_HOME', $env:XDG_DATA_HOME, 'User')
+
+$env:XDG_CONFIG_HOME = "$env:APPDATA"
+[Environment]::SetEnvironmentVariable('XDG_CONFIG_HOME', $env:XDG_CONFIG_HOME, 'User')
+
+$env:XDG_CACHE_HOME = "$env:LOCALAPPDATA\Temp"
+[Environment]::SetEnvironmentVariable('XDG_CACHE_HOME', $env:XDG_CACHE_HOME, 'User')
+
+$env:SCOOP = "$env:XDG_DATA_HOME\Scoop"
 [Environment]::SetEnvironmentVariable('SCOOP', $env:SCOOP, 'User')
 
-$env:SCOOP_GLOBAL = '%ProgramData%\Scoop'
-[Environment]::SetEnvironmentVariable('SCOOP_GLOBAL', $env:SCOOP_GLOBAL, 'Machine')
-
-$env:CARGO_HOME = '%AppData%\cargo'
+$env:CARGO_HOME = "$env:XDG_DATA_HOME\cargo"
 [Environment]::SetEnvironmentVariable('CARGO_HOME', $env:CARGO_HOME, 'User')
 
-$env:RUSTUP_HOME = '%AppData%\rustup'
+$env:GOPATH = "$env:XDG_DATA_HOME\go"
+[Environment]::SetEnvironmentVariable('GOPATH', $env:GOPATH, 'User')
+
+$env:GRADLE_USER_HOME = "$env:XDG_DATA_HOME\gradle"
+[Environment]::SetEnvironmentVariable('GRADLE_USER_HOME', $env:GRADLE_USER_HOME, 'User')
+
+$env:RUSTUP_HOME = "$env:XDG_DATA_HOME\rustup"
 [Environment]::SetEnvironmentVariable('RUSTUP_HOME', $env:RUSTUP_HOME, 'User')
+
+$env:RUSTUP_DIST_SERVER = 'https://rsproxy.cn'
+[Environment]::SetEnvironmentVariable('RUSTUP_DIST_SERVER', $env:RUSTUP_DIST_SERVER, 'User')
+
+$env:RUSTUP_UPDATE_ROOT = 'https://rsproxy.cn/rustup'
+[Environment]::SetEnvironmentVariable('RUSTUP_UPDATE_ROOT', $env:RUSTUP_UPDATE_ROOT, 'User')
+
+$env:K9SCONFIG = "$env:XDG_CONFIG_HOME\k9s"
+[Environment]::SetEnvironmentVariable('K9SCONFIG', $env:K9SCONFIG, 'User')
+
+
+
+$env:SCOOP_GLOBAL = "$env:ProgramData\Scoop"
+[Environment]::SetEnvironmentVariable('SCOOP_GLOBAL', $env:SCOOP_GLOBAL, 'Machine')
+
+$env:GOPROXY = 'https://goproxy.cn'
+[Environment]::SetEnvironmentVariable('GOPROXY', $env:GOPROXY, 'Machine')
 
 # Install packages
 
@@ -53,17 +82,17 @@ scoop install -g `
     sbt `
     scala `
     starship `
-    sudo `
     zoxide `
     zulufx8-jdk
 
 scoop install `
     FiraCode-NF `
-    JetBrainsMono-NF
+    JetBrainsMono-NF `
+    sudo
 
 iwr -useb https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim |`
     ni "$(@($env:XDG_DATA_HOME, $env:LOCALAPPDATA)[$null -eq $env:XDG_DATA_HOME])/nvim-data/site/autoload/plug.vim" -Force
 nvim +PlugInstall +qall
 
 Invoke-WebRequest https://win.rustup.rs/x86_64 -OutFile "$env:Temp\rustup.exe"
-& "$env:Temp\rustup.exe" '--default-toolchain nightly --default-toolchain nightly-x86_64-pc-windows-gnu --profile default --no-modify-path -y' | Out-Null
+& "$env:Temp\rustup.exe" '--default-toolchain nightly-x86_64-pc-windows-gnu --profile default --no-modify-path -y --component llvm-tools-preview clippy rust-analyzer-preview rust-src' | Out-Null
